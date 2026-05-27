d52a347b2e1d2a3ca2d254158816e3dbc5d60c6e
brian <brian00uni@gmail.com>
2026-05-27 15:03:53 +0900

    Configure GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/eyes/' : '/',
   plugins: [vue()],
   server: {
     proxy: {
